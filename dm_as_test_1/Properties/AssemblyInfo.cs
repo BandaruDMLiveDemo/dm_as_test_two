e9c5169273e36473475abea99de0f067ee778349
LNSBandaru <LakshmiNarasimhamBandaru@gmail.com>
2023-07-03 12:45:11 +0000

    Add project files.

---
+++ dm_as_test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dm_as_test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("dm_as_test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("90C0B08D-240F-4E2D-927E-1E166F1D6D86")]
+[assembly: AssemblyVersion("1.0.0.0")]
